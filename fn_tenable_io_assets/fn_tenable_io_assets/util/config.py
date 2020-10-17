ab051b5ea607af91d1ff00e5d701728d01cae5d7
K.Hirayama <KHRZ@jp.ibm.com>
2020-10-17 16:32:26 +0900

    Update config.py
    Parameters for Tenable.io added

--- fn_tenable_io_assets/fn_tenable_io_assets/util/config.py
+++ fn_tenable_io_assets/fn_tenable_io_assets/util/config.py
@@ -10,7 +10,11 @@ def config_section_data():
     """
     config_data = None
 
-#    config_data = u"""[fn_tenable_io_assets]
-#setting=xxx
-#"""
+    config_data = u"""[fn_tenable_io_assets]
+host_name=cloud.tenable.com
+access_key=<tenable.io access key>
+secret_key=<tenable.io secret key>
+default_scan_name=<default_scan_name>
+tio_ui_type=[ classic | new ]
+"""
     return config_data
