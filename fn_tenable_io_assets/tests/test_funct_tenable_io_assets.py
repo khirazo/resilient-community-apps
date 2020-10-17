25afdaf03c7108660509b352875b6fbde923d849
K.Hirayama <KHRZ@jp.ibm.com>
2020-10-17 22:29:52 +0900

    Update test_funct_tenable_io_assets.py
    Rewritten for local test

--- fn_tenable_io_assets/tests/test_funct_tenable_io_assets.py
+++ fn_tenable_io_assets/tests/test_funct_tenable_io_assets.py
@@ -2,77 +2,50 @@
 """Tests using pytest_resilient_circuits"""
 
 import pytest
-from resilient_circuits.util import get_config_data, get_function_definition
-from resilient_circuits import SubmitTestFunction, FunctionResult
+from resilient_lib import RequestsCommon
+from fn_tenable_io_assets.util.tenable_io import call_tenable_io
+from pprint import pprint
 
 PACKAGE_NAME = "fn_tenable_io_assets"
-FUNCTION_NAME = "tenable_io_assets"
-
-# Read the default configuration-data section from the package
-config_data = get_config_data(PACKAGE_NAME)
-
-# Provide a simulation of the Resilient REST API (uncomment to connect to a real appliance)
-resilient_mock = "pytest_resilient_circuits.BasicResilientMock"
-
-
-def call_tenable_io_assets_function(circuits, function_params, timeout=5):
-    # Create the submitTestFunction event
-    evt = SubmitTestFunction("tenable_io_assets", function_params)
-
-    # Fire a message to the function
-    circuits.manager.fire(evt)
-
-    # circuits will fire an "exception" event if an exception is raised in the FunctionComponent
-    # return this exception if it is raised
-    exception_event = circuits.watcher.wait("exception", parent=None, timeout=timeout)
-
-    if exception_event is not False:
-        exception = exception_event.args[1].args[1]
-        raise exception
-
-    # else return the FunctionComponent's results
-    else:
-        event = circuits.watcher.wait("tenable_io_assets_result", parent=evt, timeout=timeout)
-        assert event
-        assert isinstance(event.kwargs["result"], FunctionResult)
-        pytest.wait_for(event, "complete", True)
-        return event.kwargs["result"].value
-
 
 class TestTenableIoAssets:
     """ Tests for the tenable_io_assets function"""
 
-    def test_function_definition(self):
-        """ Test that the package provides customization_data that defines the function """
-        func = get_function_definition(PACKAGE_NAME, FUNCTION_NAME)
-        assert func is not None
-
-    mock_inputs_1 = {
-        "tio_operation_param2": "sample text",
-        "tio_operation_param1": "sample text",
-        "artifact_id": 123,
-        "incident_id": 123,
-        "tio_operation_type": "sample text"
+    opts = {
+        'fn_tenable_io_assets': {
+            'host_name': 'cloud.tenable.com',
+            'access_key': '',
+            'secret_key': '',
+            'default_scan_name': 'MK-LAN',
+            'tio_ui_type': 'classic'
+        }
     }
 
-    expected_results_1 = {"value": "xyz"}
-
-    mock_inputs_2 = {
-        "tio_operation_param2": "sample text",
-        "tio_operation_param1": "sample text",
-        "artifact_id": 123,
-        "incident_id": 123,
-        "tio_operation_type": "sample text"
-    }
-
-    expected_results_2 = {"value": "xyz"}
-
-    @pytest.mark.parametrize("mock_inputs, expected_results", [
-        (mock_inputs_1, expected_results_1),
-        (mock_inputs_2, expected_results_2)
+    @pytest.mark.parametrize("tio_ope, ip_addr, severity, asset_age, scan_name", [
+        ('search', '172.30.34.1,172.30.34.36', None, None, None),
+        ('search', '172.30.34.1,172.30.34.36', 'Medium,High,Critical', 120, None),
+        ('search', None, 'High,Critical', None, None),
+        ('scan_status', None, None, None, 'MK-LAN'),
+        ('scan_status', None, None, None, None)
     ])
-    def test_success(self, circuits_app, mock_inputs, expected_results):
+    def test_success(self, tio_ope, ip_addr, severity, asset_age, scan_name):
         """ Test calling with sample values for the parameters """
+        self.config = self.opts.get(PACKAGE_NAME, {})
+        req_common = RequestsCommon(opts=self.opts, function_opts=self.config)
 
-        results = call_tenable_io_assets_function(circuits_app, mock_inputs)
-        assert(expected_results == results)
+        results = call_tenable_io(self.config, req_common, tio_ope, ip_addr=ip_addr, severity=severity, asset_age=asset_age, scan_name=scan_name)
+        pprint(results)
+        assert(results['state'] == "Success")
+
+    @pytest.mark.parametrize("tio_ope, ip_addr, severity, asset_age, scan_name", [
+        ('search', None, None, None, None),
+        ('scan_status', None, None, None, 'NO_EXIST')
+    ])
+    def test_fail(self, tio_ope, ip_addr, severity, asset_age, scan_name):
+        """ Test calling with sample values for the parameters """
+        self.config = self.opts.get(PACKAGE_NAME, {})
+        req_common = RequestsCommon(opts=self.opts, function_opts=self.config)
+
+        results = call_tenable_io(self.config, req_common, tio_ope, ip_addr=ip_addr, severity=severity, asset_age=asset_age, scan_name=scan_name)
+        pprint(results)
+        assert(results['state'] == "Failed")
