121d7e1afd46c2eef6a7ada48f85f65c91769a60
K.Hirayama <KHRZ@jp.ibm.com>
2020-10-18 17:31:54 +0900

    Renamed tenable_io.py to tenable_io_lib.py
    Renamed tenable_io.py to tenable_io_lib.py to avoid confusion (tenable.io name is used in pytenable)

--- fn_tenable_io_assets/fn_tenable_io_assets/util/tenable_io_lib.py
+++ fn_tenable_io_assets/fn_tenable_io_assets/util/tenable_io_lib.py
@@ -7,12 +7,14 @@ Common lib for Tenable.io API call (via pyTenable)
 '''
 from datetime import datetime
 from tenable.io import TenableIO
-# from pprint import pprint
+from pprint import pprint, pformat
 
 TIO_RETRIES = 3
 TIO_BACKOFF = 10
-TIO_CLASSIC_URL = "https://cloud.tenable.com/app.html#/dashboards/workbench/assets/{0}/overview" 
-TIO_NEW_URL = "https://cloud.tenable.com/tio/app.html#/vulnerability-management/dashboards/default/assets/asset-details/{0}/overview"
+TIO_CLASSIC_ASSET_URL = "https://{0}/app.html#/dashboards/workbench/assets/{1}/overview" 
+TIO_NEW_ASSET_URL = "https://{0}/tio/app.html#/vulnerability-management/dashboards/default/assets/asset-details/{1}/overview"
+TIO_CLASSIC_SCAN_URL = "https://{0}/app.html#/scans/reports/{1}/history"
+TIO_NEW_SCAN_URL = "https://{0}/tio/app.html#/tools/scans/vuln-scans/scan-grid/folders/all-scans/scan-details/{1}/{2}/history"
 
 def call_tenable_io(config, req_common, tio_ope, **kwargs):
     """
@@ -24,20 +26,24 @@ def call_tenable_io(config, req_common, tio_ope, **kwargs):
             scan: ask Tenable.io to initiate a scan (for the IP addresses or default targets when omitted)
             scan_status: check scan status
         ip_addr: one ore more comma separated ip address(es)
-        severity: [ None, Low, Medium, High, Critical ]
+        severity: comma separated list of Info, Low, Medium, High, Critical combination
         asset_age: asset age in days to retrieve
         scan_name: scan name
     out: results dict
         state: Success, Failed
-        content: assets for search, uuid for scan
-        size: number of assets returned
-        reason: Failed reason
+        content: Tenable.io assets (tio_ope = search only)
+        size: number of assets returned (tio_ope = search only)
+        reason: Failed reason (state = Failed only)
+        scan_status: scan status (tio_ope = scan_status only)
+        scan_name: scan name (tio_ope = scan|scan_status only)
+        scan_uuid: scan uuid (tio_ope = scan only)
+        scan_url: scan url for Tenable.io (tio_ope = scan only)
     """
     try:
         # Get the function parameters:
         ip_addr = kwargs.get("ip_addr")  # text
         severity = kwargs.get("severity") # text
-        asset_age = kwargs.get("asset_age")  # text 
+        asset_age = kwargs.get("asset_age")  # number 
         scan_name = kwargs.get("scan_name")  # text
 
         tio = TenableIO(access_key=config.get('access_key'), secret_key=config.get('secret_key'),
@@ -70,7 +76,9 @@ def call_tenable_io(config, req_common, tio_ope, **kwargs):
                 resp = tio.scans.launch(scan_id, targets=targets)  # targets is optional
                 return {
                     'state': 'Success',
-                    'content': resp  # uuid
+                    'scan_name': scan_name,
+                    'scan_uuid': resp,  # uuid
+                    'scan_url': get_tio_scan_url(config, scan_id, resp)
                 }
             else:
                 return {
@@ -85,7 +93,8 @@ def call_tenable_io(config, req_common, tio_ope, **kwargs):
                 resp = tio.scans.status(scan_id)
                 return {
                     'state': 'Success',
-                    'content': resp
+                    'scan_name': scan_name,
+                    'scan_status': resp
                 }
             else:
                 return {
@@ -128,7 +137,7 @@ def reformat_assets(config, assets):
             out_asset['id'] = asset.get('id')
             out_asset['asset_url'] = get_tio_asset_url(config, asset.get('id'))
             out_asset['last_seen'] = asset.get('last_seen')
-            out_asset['agent_name'] = asset.get('agent_name')
+            out_asset['agent_name'] = pformat(asset.get('agent_name'))
             # get interfaces information: ipv4 & ipv6
             intfs = []
             intfs.extend(asset.get('ipv4'))
@@ -156,7 +165,9 @@ def reformat_assets(config, assets):
                         severity['high'] = sv.get('count')
                     elif sv.get('name') == 'Critical':
                         severity['critical'] = sv.get('count')
-                out_asset['severities'] = severity
+                out_asset['severities'] = "Critical: {0}\nHigh: {1}\nMedium: {2}\nLow: {3}\nInfo: {4}".format(
+                    severity.get('critical'), severity.get('high'), severity.get('medium'),
+                    severity.get('low'), severity.get('info'))
     
             out_assets.append(out_asset)
         return out_assets 
@@ -170,6 +181,14 @@ def get_tio_asset_url(config, asset_uuid):
     ui_type = config.get("tio_ui_type")  # text
 
     if ui_type.lower() == 'classic':
-        return TIO_CLASSIC_URL.format(asset_uuid)
+        return TIO_CLASSIC_ASSET_URL.format(config.get('host_name'), asset_uuid)
     else:
-        return TIO_NEW_URL.format(asset_uuid)
+        return TIO_NEW_ASSET_URL.format(config.get('host_name'), asset_uuid)
+
+def get_tio_scan_url(config, scan_id, scan_uuid):
+    ui_type = config.get("tio_ui_type")  # text
+
+    if ui_type.lower() == 'classic':
+        return TIO_CLASSIC_SCAN_URL.format(config.get('host_name'), scan_id)
+    else:
+        return TIO_NEW_SCAN_URL.format(config.get('host_name'), scan_id, scan_uuid)
